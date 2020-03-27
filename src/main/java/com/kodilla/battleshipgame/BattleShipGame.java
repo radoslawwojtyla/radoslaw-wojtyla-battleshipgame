dee5869cf8c75c695e733ee5f5d4d3e6c82c3a4f
radoslawwojtyla <radoslaw.wojtyla@gmail.com>
2020-03-27 22:50:06 +0100

    smaller background image

--- src/main/java/com/kodilla/battleshipgame/BattleShipGame.java
+++ src/main/java/com/kodilla/battleshipgame/BattleShipGame.java
@@ -1,16 +1,16 @@
 package com.kodilla.battleshipgame;
 import javafx.application.Application;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
+
 import javafx.stage.Stage;
 
 public class BattleShipGame extends Application {
 
-        private Image imageback = new Image("file:resources/image_ships.png");
+        private Image imageback = new Image("file:resources/image_ships_1280x720.png");
+
         public static void main(String[] args) {
             launch(args);
         }
@@ -18,19 +18,19 @@ public class BattleShipGame extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, true, false);
+        BackgroundSize backgroundSize = new BackgroundSize(1360, 900, true, true, true, false);
         BackgroundImage backgroundImage = new BackgroundImage(imageback, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
         Background background = new Background(backgroundImage);
 
-        GridPane gridPane = new GridPane();
-        gridPane.setBackground(background);
+        GridPane grid = new GridPane();
+        grid.setBackground(background);
 
-        Scene scene = new Scene(gridPane, 1280, 720, Color.BLACK);
-        primaryStage.setScene(scene);
+        Scene scene = new Scene(grid, 1280, 720, Color.BLACK);
 
 //        primaryStage.setResizable(false);
 //        primaryStage.setFullScreen(true);
         primaryStage.setTitle("Battle Ship - The Game");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 }
