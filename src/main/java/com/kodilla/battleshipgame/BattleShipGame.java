ee47b2f8a02f50cb4ffc8e1854db880163e09425
radoslawwojtyla <radoslaw.wojtyla@gmail.com>
2020-03-27 22:29:44 +0100

    background

--- src/main/java/com/kodilla/battleshipgame/BattleShipGame.java
+++ src/main/java/com/kodilla/battleshipgame/BattleShipGame.java
@@ -2,28 +2,38 @@ package com.kodilla.battleshipgame;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class BattleShipGame extends Application {
 
+        private Image imageback = new Image("file:resources/imageback_1600x900.png");
+
         public static void main(String[] args) {
             launch(args);
         }
 
-        @Override
-        public void start(Stage primaryStage) throws Exception {
-            Group root = new Group();
-            Scene scene = new Scene(root, 300, 300, Color.BLACK);
+    @Override
+    public void start(Stage primaryStage) throws Exception {
 
-            Rectangle r = new Rectangle(25,25,250,250);
-            r.setFill(Color.BLUE);
+        BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, true, false);
+        BackgroundImage backgroundImage = new BackgroundImage(imageback, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        Background background = new Background(backgroundImage);
 
-            root.getChildren().add(r);
-            primaryStage.setTitle("BlackJack");
-            primaryStage.setScene(scene);
-            primaryStage.show();
+        GridPane gridPane = new GridPane();
+        gridPane.setBackground(background);
 
-        }
+        Scene scene = new Scene(gridPane, 1280, 720, Color.BLACK);
+        primaryStage.setScene(scene);
+
+        primaryStage.setResizable(false);
+//        primaryStage.setFullScreen(true);
+        primaryStage.setTitle("Battle Ship - The Game");
+
+        primaryStage.show();
+
     }
+}
